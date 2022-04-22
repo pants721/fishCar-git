b2b67d3f8cb57eeb8ba7da2faae54477d95eb06c
Lucas Newcomb <94380471+pants721@users.noreply.github.com>
2022-04-22 10:49:50 -0400

    Black formatted main.py

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ cap = cv2.VideoCapture("fish.mp4")
 # Globals
 font = cv2.FONT_HERSHEY_SIMPLEX
 # Use this
-padding = 200 
+padding = 200
 # Or
-paddingWidth = 200 
+paddingWidth = 200
 paddingHeight = 200
 
+
 def angleBetween(p1, p2):
-    """Calculates the angle at which the center of the fish is offset from the origin 
+    """Calculates the angle at which the center of the fish is offset from the origin
 
     Args:
         p1 (tuple): coordinates to point one
@@ -29,6 +30,7 @@ def angleBetween(p1, p2):
     rads = math.atan2(p2[1] - p1[1], p2[0] - p1[0])
     # Returns precise but in reference is rounded
     return math.degrees(rads)
+
 
 def determineDir(angle):
     """Function to determine which of the eight cardinal directions our angle corelates to
@@ -52,25 +54,26 @@ def determineDir(angle):
     # NW = -157.5 - -112.5
     try:
         if -112.5 <= angle <= -67.5:
-            return 'N'
+            return "N"
         elif -67.5 <= angle <= -22.5:
-            return 'NE'
+            return "NE"
         elif -22.5 <= angle <= 22.5:
-            return 'E'
+            return "E"
         elif 22.5 <= angle <= 67.5:
-            return 'SE'
+            return "SE"
         elif 67.5 <= angle <= 112.5:
-            return 'S'
+            return "S"
         elif 112.5 <= angle <= 157.5:
-            return 'SW'
+            return "SW"
         elif 157.5 <= angle <= 180 or -157.5 <= angle <= -112.5:
-            return 'W'
+            return "W"
         elif 157.5 <= angle <= -112.5:
-            return 'NW'
+            return "NW"
         else:
             return "COCK"
     except:
         raise Exception("Error in determineDir()")
+
 
 def outOfEdge(x, y, w, h, leftEdge, rightEdge, topEdge, bottomEdge):
     """A function to determine if the center of our fish is out of the bounds of the center padding box
@@ -88,8 +91,8 @@ def outOfEdge(x, y, w, h, leftEdge, rightEdge, topEdge, bottomEdge):
     Returns:
         bool: True if the fish is out of bounds, False if not
     """
-    centerX = (x + (x+w)) / 2
-    centerY = (y + (y+h)) / 2
+    centerX = (x + (x + w)) / 2
+    centerY = (y + (y + h)) / 2
     if centerX > rightEdge or centerX < leftEdge:
         return True
     if centerY > topEdge or centerY < bottomEdge:
@@ -97,7 +100,8 @@ def outOfEdge(x, y, w, h, leftEdge, rightEdge, topEdge, bottomEdge):
     else:
         return False
 
-while(1):
+
+while 1:
     # Frame and color vars
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -115,9 +119,7 @@ while(1):
     res = cv2.bitwise_and(frame, frame, mask=mask)
 
     # Contours
-    contours, hier = cv2.findContours(mask,
-                                      cv2.RETR_TREE,
-                                      cv2.CHAIN_APPROX_SIMPLE)
+    contours, hier = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     # Establish win height, width, and idk
     winH, winW, winC = frame.shape
@@ -134,8 +136,8 @@ while(1):
             # Establishes coordinates (x, y) and dimensions (w, h) of our fish's bounding rectangle
             x, y, w, h = cv2.boundingRect(c)
             # Useful shorthands
-            centerX = (x + (x+w)) / 2
-            centerY = (y + (y+h)) / 2
+            centerX = (x + (x + w)) / 2
+            centerY = (y + (y + h)) / 2
             centerH = winH / 2
             centerW = winW / 2
             # I have to use these vars for angle idk why
@@ -143,7 +145,7 @@ while(1):
             point2 = (centerX, centerY)
 
             # Draws our bounding box
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 5)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 5)
 
             angle = angleBetween(point1, point2)
             dir = determineDir(angle)
@@ -151,18 +153,24 @@ while(1):
             # print(dir, np.round(angle))
 
             # Draws line from center to fish
-            cv2.line(frame, (int(centerW), int(centerH)), (int(centerX), int(centerY)), (0, 255, 0), 5)
+            cv2.line(
+                frame,
+                (int(centerW), int(centerH)),
+                (int(centerX), int(centerY)),
+                (0, 255, 0),
+                5,
+            )
             # Establishes a ratio so the bounding box is proportionate
             # Use if wanted
             # gcd = np.gcd(winW, winH)
-            # wRatio = winW / gcd 
+            # wRatio = winW / gcd
             # hRatio = winH / gcd
             # paddingWidth = padding * (wRatio * 0.1)
             # paddingHeight = padding * (hRatio * 0.1)
 
             # Center of window
             centerW, centerH = winW / 2, winH / 2
-            
+
             # Edges
             leftEdge = centerW - paddingWidth
             rightEdge = centerW + paddingWidth
@@ -184,16 +192,20 @@ while(1):
                 inBounds = True
 
             # !DEBUG
-            # Center Box 
-            cv2.rectangle(frame, topLeftCenterBounds, bottomRightCenterBounds, (0, 255, 0), 5)
-            cv2.putText(frame, textDisplayValue, (100, 100), font, 1, (255, 255, 255), 5)
+            # Center Box
+            cv2.rectangle(
+                frame, topLeftCenterBounds, bottomRightCenterBounds, (0, 255, 0), 5
+            )
+            cv2.putText(
+                frame, textDisplayValue, (100, 100), font, 1, (255, 255, 255), 5
+            )
 
-    cv2.imshow('frame', frame)
-        # cv2.imshow('mask', mask)
-        # cv2.imshow('res', res)
+    cv2.imshow("frame", frame)
+    # cv2.imshow('mask', mask)
+    # cv2.imshow('res', res)
 
     frameTime = 10
-    if cv2.waitKey(frameTime) & 0xFF == ord('q'):
+    if cv2.waitKey(frameTime) & 0xFF == ord("q"):
         cap.release()
         cv2.destroyAllWindows()
         break
